3e43c7cf62769ed916f52ed8d8ff33adc8c3167a
王春秋 <ikaros109@163.com>
2019-12-15 13:29:08 +0800

    删除权限

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -21,7 +21,29 @@
         <!--  展开列  -->
         <el-table-column align="center" type="expand">
           <template slot-scope="scope">
-            {{scope.row.children}}
+            <!-- <pre>
+             {{scope.row}}
+           </pre> -->
+            <el-row :class="['bdbottom', i1===0 ? 'bdtop' : '']" v-for="(item1, i1) in scope.row.children"
+                    :key="item1.id">
+              <!--  一级权限  -->
+              <el-col :span="5">
+                <el-tag>{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 二级权限和三级权限   -->
+              <el-col :span="19">
+                <el-row :class="i2 === 0 ? '' : 'bdtop'" v-for="(item2, i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success">{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag type="warning" v-for="(item3) in item2.children" :key="item3.id" closable @close="delRightById(scope.row, item3.id)">{{item3.authName}}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
           </template>
         </el-table-column>
         <!--  索引列  -->
@@ -60,11 +82,40 @@
         if (result.meta.status !== 200) return this.$message.error(result.meta.msg)
         this.roleList = result.data
         console.log(result.data)
+      },
+      // 根据id删除对应的权限
+      async delRightById(role, rightId) {
+        const confirmResult = await this.$confirm(
+          '此操作将永久删除该文件, 是否继续?',
+          '提示',
+          {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }
+        ).catch(err => err)
+        if (confirmResult !== 'confirm') {
+          return this.$message.info('取消了删除！')
+        }
+        const { data: result } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+        if (result.meta.status !== 200) return this.$message.error(result.meta.msg)
+        console.log(result)
+        role.children = result.data
       }
     }
   }
 </script>
 
 <style lang="less" scoped>
+  .el-tag {
+    margin: 7px;
+  }
 
+  .bdbottom {
+    border-bottom: 1px solid #eee;
+  }
+
+  .bdtop {
+    border-top: 1px solid #eee;
+  }
 </style>
