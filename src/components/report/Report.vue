b0a8edaba8000238ee7f72916508ce06fa53a96a
王春秋 <ikaros109@163.com>
2019-12-19 14:33:42 +0800

    报表初始化

---
+++ src/components/report/Report.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!--  面包屑导航区域  -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--  卡片区域  -->
+    <el-card>
+      <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
+      <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  // 导入echarts组件
+  import echarts from 'echarts'
+  export default {}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import Goods from '../components/goods/Goods'
 import Add from '../components/goods/Add'
 import Order from '../components/orders/Order'
+import Report from '../components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -33,7 +34,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: Goods },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 
